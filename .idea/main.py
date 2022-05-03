804f2f137f575cf7b363010395049f011b7fe193
hamada <hamada.devlop@gmail.com>
2022-05-03 03:00:18 +0200

    initial

---
+++ .idea/main.py
@@ -0,0 +1,18 @@
+name = "hanin"
+classname = 6
+print(classname)
+
+
+def print_name(text):
+    print(text)
+
+
+def example_if(number):
+    if number % 2:
+        print("رقم فردي")
+    else:
+        print('رقم زوجي')
+
+
+print_name('hamada')
+example_if(1)
